96876a64bfa57c65b66631c7159f9f3c963d6736
RaimundoJSoares <raianimes1000@gmail.com>
2022-08-02 19:39:58 -0400

    continuando projeto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,7 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { zodResolver} from '@hookform/resolvers/zod'
+import * as zod from "zod";
 import {
   ButtonStyles,
   CountDownStyles,
@@ -8,17 +11,67 @@ import {
   TaskInputStyles,
   TimerInputStyles,
 } from "./HomeStyles";
+import { useState } from "react";
+
+const newCycleFormValidationSchema = zod.object({
+ task: zod.string().min(1, 'Informe uma tarefa'),
+ minutesAmount : zod.number().min(5).max(60)
+})
+
+//interface newCycleFormData {
+  //task: string;
+  //minutesAmount: number;
+//}
+
+type newCycleFormData = zod.infer<typeof  newCycleFormValidationSchema>
+
+interface Cycleprops {
+  task: string;
+  minutesAmount: number;
+  id: string;
+}
 
 export function Home() {
+  const [cycles, setCycles] = useState<Cycleprops[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>( null)
+
+  const {register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+    task: '',
+    minutesAmount: 0,
+    },
+  });
+
+  function handleCreateNewCycle(data : newCycleFormData) {
+    const id =  String(new Date().getTime())
+    const newCycle : Cycleprops = {
+
+      id,
+      task : data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle]) //adiciona o novo ciclo no array de ciclos
+    setActiveCycleId(id)
+
+    reset()
+  }
+  const task  = watch('task') //controlled input component
+  const isSubmitDisabled = !task;
+
+
+
   return (
     <HomeStyles>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormStyles>
           <label htmlFor="task">Vou trabalhar em: </label>
           <TaskInputStyles
            id="task" 
            placeholder="Dê um nome ao seu projeto" 
             list="task-suggestions"
+           {...register("task")} // register the input, task é o nome que eu dei ao input
            />
 
           <datalist id="task-suggestions">
@@ -34,6 +87,7 @@ export function Home() {
            step={5}
             min={5}
             max={60}
+            {...register("minutesAmount", {valueAsNumber:true})} // register the input, minutesAmount é o nome que eu dei ao input
            />
 
           <span>minutos.</span>
@@ -46,7 +100,7 @@ export function Home() {
           <span>0</span>
         </CountDownStyles>
 
-        <ButtonStyles disabled type="submit">
+        <ButtonStyles disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </ButtonStyles>
